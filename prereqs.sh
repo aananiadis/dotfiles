02d0645fd49e62c193e847ec812e49f9334da326
Alexander Ananiadis <3892795+aananiadis@users.noreply.github.com>
2020-04-21 20:40:32 -0400

    Submodules

--- prereqs.sh
+++ prereqs.sh
@@ -6,6 +6,5 @@ if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
-  echo 'Updating oh-my-zsh'
-  /bin/zsh $ZSH/tools/upgrade.sh
+  echo 'oh-my-zsh installed, update with: `upgrade_oh_my_zsh`'
 fi
