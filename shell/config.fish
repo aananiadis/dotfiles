73f086dc6899602f3e5938c90e235aa0d5c20613
Alexander Ananiadis <agananiadis@gmail.com>
2026-04-07 01:14:36 -0400

    Add git abbreviations, env vars, and fix tmux keybindings
    - Expand shell abbreviations with git shortcuts and better ls flags
    - Add VOLTA_HOME, EDITOR, and PATH entries to fish config
    - Change tmux window switch bindings from M-Arrow to C-M-Arrow

--- shell/config.fish
+++ shell/config.fish
@@ -4,6 +4,13 @@ set -U fish_greeting
 # brew init
 [ ! -f /opt/homebrew/bin/brew ] || eval "$(/opt/homebrew/bin/brew shellenv)"
 
+set -gx VOLTA_HOME $HOME/.volta
+set -gx EDITOR "cursor --wait"
+
+# PATH update
+fish_add_path $VOLTA_HOME/bin
+fish_add_path ~/.local/bin
+
 
 # function starship_transient_prompt_func
 #   starship module character
@@ -12,7 +19,6 @@ set -U fish_greeting
 #   starship module time
 # end
 
+# Starship
 starship init fish | source
 enable_transience
-# # Starship
-# starship init fish | source
